84a99c4583cb90000e2373624ef700b893576583
To Ka <me@toka.ge>
2021-05-09 20:00:44 +0400

    Log reload initiation

--- AcraWebsite/Jobs/RefreshDataJob.cs
+++ AcraWebsite/Jobs/RefreshDataJob.cs
@@ -1,21 +1,26 @@
 ﻿using AcraWebsite.Caching;
+using Microsoft.Extensions.Logging;
 
 namespace AcraWebsite.Jobs
 {
     public class RefreshDataJob
     {
         private readonly IBookingDataOverviewCache _bookingDataOverviewCache;
+        private readonly ILogger<RefreshDataJob> _logger;
 
         public RefreshDataJob(
-            IBookingDataOverviewCache bookingDataOverviewCache
+            IBookingDataOverviewCache bookingDataOverviewCache,
+            ILogger<RefreshDataJob> logger
             )
         {
             _bookingDataOverviewCache = bookingDataOverviewCache;
+            _logger = logger;
         }
 
         public void Process()
         {
             _bookingDataOverviewCache.InitiateDataReload();
+            _logger.LogInformation("Data reload initiated");
         }
     }
 }
